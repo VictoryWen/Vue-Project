095c89261cd08b3c24c4ac9fc854232cbe784001
xuwen <superman1969@sina.com>
2018-02-08 18:01:42 +0800

    simple ep

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="hello">
-  <p>{{msg}}</p>
+    <p>{{msg}}</p>
     <button @click="reverseMessage">逆转消息</button>
+
+    <span :title="message">
+      鼠标悬停几秒钟查看此处动态绑定的提示信息！
+    </span>
+
+    <p v-if="seen">你现在看见我了</p>
+
+    <ol>
+      <li v-for="todo in todos">
+        {{ todo.text }}
+      </li>
+    </ol>
+
+    <p>{{message}}}</p>
+
   </div>
+
 </template>
 
 <script>
@@ -10,7 +26,14 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      message: '页面加载于' + new Date().toDateString(),
+      seen: false,
+      todos: [
+        {text: '学习 JavaScript'},
+        {text: '学习 Vue'},
+        {text: '学习 efefv'}
+      ]
     }
   },
   methods: {
